bf668279c7d93695d4b02431b413ce5281d0b321
Adarsh <herecomesthebeastlesnar@gmail.com>
2020-09-15 14:34:32 +0530

    beta 4..0

--- app.js
+++ app.js
@@ -4,16 +4,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello");
-});
-
-app.get("/ip", (req, res) => {
   const ip =
     req.headers["x-forwarded-for"] ||
     req.connection.remoteAddress ||
     req.socket.remoteAddress ||
     (req.connection.socket ? req.connection.socket.remoteAddress : null);
-  res.send(`<h1>${ip}</h1>`);
+  res.send(`<h1>IP Address: ${ip}</h1><br/><h1>Header: ${req.headers.user-agent}</h1>`);
   console.log(req.connection.remoteAddress);
 });
 
